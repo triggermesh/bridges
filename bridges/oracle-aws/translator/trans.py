f20e26a013fcfdd6f02b5888380158f14770ef35
Chris Baumbauer <cab@cabnetworks.net>
2020-06-12 13:51:24 -0700

    Cleanup based on PR feedback, renaming, and fixing trigger filters

--- bridges/oracle-aws/translator/trans.py
+++ bridges/oracle-aws/translator/trans.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 from flask import Flask
 from flask import request
 
@@ -13,20 +12,20 @@ app = Flask(__name__)
 def trans():
     ce = request.get_json(force=True)
     print(request.data)
-    
+
     # respond with a cloudevent
     headers = {}
-    headers['ce-specversion']='1.0'
-    headers['ce-time']='2018-04-05T03:56:24Z'
-    headers['ce-id']='XXX-YYY-ZZZ-WWW'
-    headers['content-type']='application/json'
-    headers['ce-type']='com.triggermesh.transform'
-    headers['ce-source']='oracle-source'
-    
-    # FOR SQS
-    body = { "message": "missing ce data"}
+    headers['Ce-Specversion']='1.0'
+    headers['Ce-Time']=request.headers['Ce-Time']
+    headers['Ce-Id']=request.headers['Ce-Id']
+    headers['Content-Type']='application/json'
+    headers['Ce-Type']='com.triggermesh.targets.oracledb.fn.dumper'
+    headers['Ce-Source']='translators.triggermesh.io/oracle-source-translator'
 
-    if ce is not None:
+    # FOR SQS
+    body = { "message": "missing oracle db change data"}
+
+    if ce is not None and "op" in ce:
         if ce["op"] == "INSERT":
             body = { "message": "A new user wants to say something: " + ce["new"]["DATA"] }
         elif ce["op"] == "DELETE":
@@ -34,13 +33,21 @@ def trans():
         else:
             body = { "message": "Don't know what to do with this: " + json.dumps(ce) }
 
-    print(body)
-    response = app.response_class(
+        return app.response_class(
             response=json.dumps(body),
             headers=headers,
             status=200,
             mimetype='application/json'
     )
+    else:
+        print("Unkown event, skipping...")
+
+        return app.response_class(
+            response=json.dumps(body),
+            headers=headers,
+            status=500,
+            mimetype='application/json'
+        )
 
     return response 
 

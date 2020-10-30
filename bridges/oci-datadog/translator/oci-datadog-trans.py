f2cf76da67cc9fb430cdb0c5540ea32dae3a82f2
Chris Baumbauer <cab@cabnetworks.net>
2020-10-30 09:36:21 -0700

    Refactor and fix issues with oci->datadog translator

--- bridges/oci-datadog/translator/oci-datadog-trans.py
+++ bridges/oci-datadog/translator/oci-datadog-trans.py
@@ -10,19 +10,19 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def trans():
-    ce = request.get_json(force=True)
-    print(request.data)
+    try:
+        ce = request.get_json(force=True)
+        print(request.data)
 
-    # respond with a cloudevent
-    headers = {}
-    headers['Ce-Specversion']='1.0'
-    headers['Ce-Time']=request.headers['Ce-Time']
-    headers['Ce-Id']=request.headers['Ce-Id']
-    headers['Ce-Type']='io.triggermesh.datadog.metric.aggregated'
-    headers['Ce-Source']=request.headers['Ce-Source'] + "/translated"
+        # respond with a cloudevent
+        headers = {}
+        headers['Ce-Specversion']='1.0'
+        headers['Ce-Time']=request.headers['Ce-Time']
+        headers['Ce-Id']=request.headers['Ce-Id']
+        headers['Ce-Type']='io.triggermesh.datadog.metric.aggregated'
+        headers['Ce-Source']=request.headers['Ce-Source'] + "/translated"
 
-    # Modify the Oracle OCI Monitoring data to be usable for Datadog
-    if ce is not None:
+        # Modify the Oracle OCI Monitoring data to be usable for Datadog
         data = []
         for e in ce:
             displayName = e['name']
@@ -50,15 +50,15 @@ def trans():
             status=200,
             mimetype='application/json'
         )
-    else:
-        print("Unkown event, skipping...")
-
+    except:
+        print("Invalid event")
+        body = {}
+        body['msg'] = "Unable to read event"
         return app.response_class(
             response=json.dumps(body),
-            headers=headers,
-            status=500,
-            mimetype='application/json'
-        )
+            status=400,
+            mimetype='application/json')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)

1219dbfc68458c5ec48d8f1175ffc5bb4b76fd6e
Jeff <60796676+JeffNeff@users.noreply.github.com>
2021-02-02 17:11:33 -0700

    Update Zendesk Tag Event Type (#87)

--- bridges/zendesk-zendesk/transform-sentiment-tag/receiver.go
+++ bridges/zendesk-zendesk/transform-sentiment-tag/receiver.go
@@ -31,7 +31,7 @@ import (
 
 const (
 	ticketCreatedEventType = "com.zendesk.ticket.created"
-	tagCreateEventType     = "com.zendesk.tag.create"
+	tagCreateEventType     = "com.zendesk.ticket.tag.add"
 
 	transformTypeExtension = "transformtype"
 )
